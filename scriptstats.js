6a0392a546f9f661276891e52b80ce5f07bbee50
Jricnav1811 <jricnav1811@g.educaand.es>
2024-02-16 13:32:07 +0100

    Datos Especie, Puntos base Especie
    Cambios:
    1º Se añade especies para descripción del pokémon.
    2º Se crean variables para descripción y su traducción.
    3º Se crea la variable categoría.
    3º Se crea la variable puntosBaseContainer y se le añaden todos los datos correspondientes al html.

--- scriptstats.js
+++ scriptstats.js
@@ -4,9 +4,25 @@ async function obtenerPokemons() {
   const parametroUrl = new URLSearchParams(window.location.search);
   const id = parametroUrl.get('id');
 
+
   if (id) {
     const pokemon = await getData(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    draw(pokemon);
+
+    const species = await getData(pokemon.species.url); // NUEVO @JAVI
+    const puntosBase = await getData(`https://pokeapi.co/api/v2/pokemon-species/${id}`); // NUEVO @JAVI
+
+    // Filtrar para obtener la descripción en español
+    let textoEspañol = species.flavor_text_entries.filter(entry => entry.language.name === 'es'); // NUEVO @JAVI
+
+    // Devolver la descripción
+    let description; // NUEVO @JAVI
+    if (textoEspañol.length > 0) { // NUEVO @JAVI 
+      description = textoEspañol[0].flavor_text; // NUEVO @JAVI
+    } else { // NUEVO @JAVI
+      description = 'No se encontró descripción en español para este Pokémon.'; // NUEVO @JAVI
+    } // NUEVO @JAVI
+
+    draw(pokemon, species, description, puntosBase);
   } else {
     console.error('No se encontró el parámetro "id" en la URL');
   }
@@ -15,14 +31,7 @@ async function obtenerPokemons() {
 obtenerPokemons();
 
 
-async function draw(pokemon) {
-
-  let tipos = '';
-  if (pokemon.types.length == 2) {
-    tipos += pokemon.types[0].type.name + ' ' + pokemon.types[1].type.name;
-  } else {
-    tipos += pokemon.types[0].type.name;
-  }
+async function draw(pokemon, species, description, puntosBase) { // NUEVO @JAVI
 
   // Crear una nueva carta de Pokémon
   let nuevaCarta = document.createElement('div');
@@ -30,16 +39,54 @@ async function draw(pokemon) {
   nuevaCarta.innerHTML = `
       <div class="pokemon-imagen">
         <img src="${pokemon.sprites.other['official-artwork'].front_default}">
-      </div>
-      <div class="bloque-pokemon">
-        <div class="pokemon-nombre">${pokemon.name}</div>
-        <div class="pokemon-id">${pokemon.id}</div>
-        <div class="pokemon-tipos">${tipos}</div>
       </div>`;
 
   // Agregar la carta de Pokémon al contenedor
   detallesPokemon.appendChild(nuevaCarta);
+
+  // A PARTIR DE AQUÍ ES TODO LO QUE HE HECHO @Javi
+
+  let categoria = species.genera && species.genera.filter(g => g.language.name === 'es')[0] ? species.genera.filter(g => g.language.name === 'es')[0].genus : 'No disponible';
+
+  let descripcionPokemon = document.createElement('div');
+  descripcionPokemon.className = 'descripcion-pokemon';
+  descripcionPokemon.innerHTML = `
+      <h1 class="nombre-pokemon">${pokemon.name}</h1>
+      <h3 class="numero-pokemon">Nº: ${pokemon.id}</h3>
+      <p class="parrafo-pokemon">${description}</p>
+      <div class="contenedor-datos">
+        <p><b>Altura</b>: ${pokemon.height}</p>
+        <p><b>Categoría</b>: ${categoria}</p>
+        <p><b>Peso</b>: ${pokemon.weight}</p>
+        <p><b>Habilidad</b>: ${pokemon.abilities[0].ability.name}</p>
+        <p><b>Sexo</b>: ${species.gender_rate}</p>
+      </div>`;
+
+  detallesPokemon.appendChild(descripcionPokemon);
+
+  let puntosBaseContainer = document.createElement('div');
+  puntosBaseContainer.className = 'puntos-base';
+  puntosBaseContainer.innerHTML = `
+      <h3>Puntos Base</h3>
+      <div class="contenedor-puntos">
+        <p class="puntos-vida">Vida: ${pokemon.stats[0].base_stat}</p>
+        <p class="puntos-ataque">Ataque: ${pokemon.stats[1].base_stat}</p>
+        <p class="puntos-defensa">Defensa: ${pokemon.stats[2].base_stat}</p>
+        <p class="puntos-ataque-especial">Ataque Especial: ${pokemon.stats[3].base_stat}</p>
+        <p class="puntos-defensa-especial">Defensa Especial: ${pokemon.stats[4].base_stat}</p>
+        <p class="puntos-velocidad">Velocidad: ${pokemon.stats[5].base_stat}</p>
+      </div>`;
+
+  detallesPokemon.appendChild(puntosBaseContainer);
+
 }
+
+// let tipos = '';
+//   if (pokemon.types.length == 2) {
+//     tipos += pokemon.types[0].type.name + ' ' + pokemon.types[1].type.name;
+//   } else {
+//     tipos += pokemon.types[0].type.name;
+//   }
 
 
 
